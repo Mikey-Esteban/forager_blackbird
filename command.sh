eef848a05bd8d515772de06667874af152d8f236
mikey-esteban <michaelp.esteban@gmail.com>
2022-09-23 13:39:57 -0400

    edited logo and link

---
+++ command.sh
@@ -0,0 +1,13 @@
+# create react app 
+npx create-react-app blackbird
+
+# after creating github repo, init and commit
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Mikey-Esteban/forager_blackbird.git
+git push -u origin main
+
+# switch branch to 'update_logo'
+git checkout -b update_logo
